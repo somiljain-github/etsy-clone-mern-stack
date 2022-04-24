93c79a020b5b7ed6119a36fcea618eba543d8b71
somil <somilj94@gmail.com>
2022-04-24 14:04:40 -0700

    bug fixes

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -47,14 +47,17 @@ function Cart() {
   const placeOrder = (e) => {
     e.preventDefault();
     console.log("the items are", items);
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
     items.map(item => {
+      item.dop = today;
       item.itemID = item._id;
       delete item._id;
       delete item.category;
       delete item.description;
-      delete item.displayPicture;
-      delete item.name;
-      delete item.price;
       delete item.salesCount;
       delete item.userID;
       delete item.__v;
