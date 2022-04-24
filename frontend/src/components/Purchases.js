235bdf34a90c1991ce43132c644d9a8d8da8225b
Somil Jain <somilj94@gmail.com>
2022-04-24 12:57:43 -0700

    pagination in purchases

--- frontend/src/components/Purchases.js
+++ frontend/src/components/Purchases.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Form } from 'react-bootstrap';
 import Navbar from "./Navbar";
 import axios from "axios";
 import { constants } from "../config/config";
 import authHeader from "../services/authHeader";
-import '../styles/Purchases.css'
+import '../styles/Purchases.css';
+import { PaginatedList } from 'react-paginated-list';
 
 function Purchases() {
-  const userID = localStorage.getItem("userID");
+//   const userID = localStorage.getItem("userID");
   var [purchases, setPurchases] = useState([]);
-//   const userID = '625128a2ad676dcca6e551bf';
+  var [pageSize, setPageSize] = useState([]);
+  var [i, setI] = useState([]);
+  const userID = '625128a2ad676dcca6e551bf';
   const URL = `http://${constants.IP.ipAddress}:3001/api/v1/order/getOrders/${userID}`;
   console.log(URL);
 
@@ -34,64 +38,148 @@ function Purchases() {
       .catch((err) => {
         console.log("error in getting all items in the home page", err);
       });
+
+      pageSize = 5;
+      setPageSize(pageSize)
+    
+      i = 0;
+      setI(0);
+
+    //   console.log('@#@@@@@@@@@', i, pageSize)
+
   }, []);
 
   console.log('$#$# purchases: ', purchases);
-  let i = 0;
-  const purchase_cards = purchases.map((purchase) => {
+//   let i = 0;
+//   const purchase_cards = {
+//     return(purchases)}
+      //purchases.map((purchase) => {
       
-        console.log('#####$$$$$ ', purchase, ' | i = ', i)
+        // console.log('#####$$$$$ ', purchase, ' | i = ', i)
 
-        i += 1
+        // i += 1
 
-        let item_no = 0
-        const items = purchase.items.map((item) => {
-            
-            item_no += 1
-            return (
-                <div className="item-card" key={JSON.stringify(item) + item_no}>
-                    <p><span className="item-head">Item {item_no}</span> <span className="item_id_top_right">{item.itemID}</span></p>
+        // let item_no = 0
+        // const items = purchase.items.map((item) => {
+        //     item_no += 1
+        //     return (
+        //         <div className="item-card" key={JSON.stringify(item) + item_no}>
+        //             <p><span className="item-head">Item {item_no}</span> <span className="item_id_top_right">{item.itemID}</span></p>
 
-                    <div key={JSON.stringify(item)} className="item-inner-box">
-                        <div className="row">
-                            <div className="col-md-8 item-left-box">
-                                {/* <div>{JSON.stringify(item)}</div> */}
-                                <p>Qty: {item.quantity}</p>
-                                <p>{item.isGiftPack ? 
-                                <span><p>Gift wrapped!</p>
-                                    <p>{item.instructions !== "" ? item.instructions : ""}</p>
-                                </span> 
-                                : ''}</p>
-                            </div>
-                            <div className="col-md-4 item-right-box">
-                                {/* image container */}
-                                <p>test</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-        })
-        
-        return (
-            <div className="purchase-card">
-                <p>Purchase {i}</p>
-                <br/>
-                {items}
-            </div>
-        )
-  });
+        //             <div key={JSON.stringify(item)} className="item-inner-box">
+        //                 <div className="row">
+        //                     <div className="col-md-8 item-left-box">
+        //                         {/* <div>{JSON.stringify(item)}</div> */}
+        //                         <p>Qty: {item.quantity}</p>
+        //                         <p>{item.isGiftPack ? <p>Gift wrapped!</p> : ''}</p>
+        //                     </div>
+        //                     <div className="col-md-4 item-right-box">
+        //                         {/* image container */}
+        //                         <p>test</p>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     )
+        // })
+        // return (
+        //     <div className="purchase-card">
+        //         <p>Purchase {i}</p>
+        //         <br/>
+        //         {items}
+        //     </div>
+        // )
+  //});
+
+//   i = 0;
+
 
   return (
     <div>
         <Navbar />
         <div className="container">
             <div>Purchases</div>
-            <div>
-                {purchase_cards}
+             <div>
+                {/* {purchase_cards} */}
+
+                {/* <PaginatedList
+                    list={lmain}
+                    itemsPerPage={5}
+                    renderList={(list) => (list.map(order =>(<>{console.log(order)}<OrderTile orders={order}></OrderTile></>)))}
+                    ></PaginatedList> */}
             </div>
+
+            {/* <PaginatedList
+            list={purchases}
+            itemsPerPage={5}
+            renderList={(list) => (list.map(purchase =>(<>{JSON.stringify(purchase)}</>)))}
+            ></PaginatedList> */}
+
+            {/* <PaginatedList
+            list={purchases}
+            itemsPerPage={5}
+            renderList={(list) => (list.map(purchase =>(<>{
+                <div className="purchase-card" key={JSON.stringify(purchase)}>
+                    <p>Purchase {i}</p>
+                    <br/>
+                    {JSON.stringify(purchase)}
+                </div>
+            }</>)))}
+            ></PaginatedList> */}
+
+
+            
+            {/* <div className="page-size-selection">
+                {/* <Form.Select size="sm" className="sort-filter" value={sortBy} onChange={(e)=>{setSortBy(e.target.value)}} id="sort-homeitem" name="sort-homeitem">
+                  <option value="2">2</option>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                </Form.Select> }
+                
+            </div> */}
+
+            <Form.Select size="sm" className="sort-filter" onChange={(e)=>{setPageSize(parseInt(e.target.value))}} id="sort-homeitem" name="sort-homeitem">
+                  <option value="2">2</option>
+                  <option selected value="5">5</option>
+                  <option value="10">10</option>
+            </Form.Select>
+
+            <PaginatedList
+                list={purchases}
+                itemsPerPage={pageSize}
+                renderList={(list) => (list.map(purchase =>(<>{
+                    <div className="purchase-card" key={JSON.stringify(purchase)}>
+                        <p> <span className="item_id_top_right"><b>Purchase Id:</b> {purchase._id}</span></p>
+                        <br/>
+                        {
+                            (purchase.items.map(item =>(<>
+                            {/* {console.log(item)} */}
+                                <div className="item-card" key={JSON.stringify(item)}>
+                                    <p><span className="item-head"></span> <span className="item_id_top_right">{item.itemID}</span></p>
+
+                                    <div key={JSON.stringify(item)} className="item-inner-box">
+                                        <div className="row">
+                                            <div className="col-md-8 item-left-box">
+                                                {/* <div>{JSON.stringify(item)}</div> */}
+                                                <p>Qty: {item.quantity}</p>
+                                                <p>{item.isGiftPack ? <p>Gift wrapped!</p> : ''}</p>
+                                            </div>
+                                            <div className="col-md-4 item-right-box">
+                                                {/* image container */}
+                                                {/* <p>test</p> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>)))
+                        }
+                    </div>
+                }</>)))}
+            ></PaginatedList>
+
+
         </div>
-    </div>
+        </div>
   )
 }
 
